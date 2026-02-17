c5be31021deb26d89f1e7a40b25342be1145f0b1
Krrish2401 <krrish.231@lpu.in>
2026-02-17 16:35:36 +0530

    iframe setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import Home from "./pages/home.jsx"
+import { Routes, Route, Link } from 'react-router-dom'
+import { Home } from './pages/home.jsx'
+import { Watch } from './pages/watch.jsx'
 import './App.css'
 
 function App() {
-  return(
-    <Layout>
-      <Routes>
-        <Route path = "/" element={<Home/>}/>
-        <Route path = "/watch/:id" element={<Watch/>}/>
-        <Route path = "/upload" element={<Upload/>}/>
-        <Route path = "/profile" element={<Profile/>}/>
-        <Route path = "/search" element={<Search/>}/>
-      </Routes>
-    </Layout>
+  return (
+    <div className="app">
+      <header className="navbar">
+        <Link to="/" className="logo-link">
+          <h1 className="logo-text">MediaStreamer</h1>
+        </Link>
+        <nav className="nav-links">
+          <Link to="/" className="nav-link">Home</Link>
+        </nav>
+      </header>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/watch/:id" element={<Watch />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
